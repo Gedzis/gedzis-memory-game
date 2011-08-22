b2a434bfeb3e96560def55586cc957041abd3fbe
gedisu@gmail.com <gedisu@gmail.com@60d33090-e776-6669-c13a-5299a5993b29>
2011-08-22 07:09:21 +0000

    Pirma versija. Veikia paprasciausia funkcija

---
+++ branches/0.1/src/net/gedzis/memory/common/Common.java
@@ -0,0 +1,39 @@
+package net.gedzis.memory.common;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.gedzis.memory.R;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+
+public class Common {
+	public List<Drawable> loadImages(Context context) {
+		List<Drawable> images = new ArrayList<Drawable>();
+		images.add(context.getResources().getDrawable(R.drawable.card1));
+		images.add(context.getResources().getDrawable(R.drawable.card2));
+		images.add(context.getResources().getDrawable(R.drawable.card3));
+		images.add(context.getResources().getDrawable(R.drawable.card4));
+		images.add(context.getResources().getDrawable(R.drawable.card5));
+		images.add(context.getResources().getDrawable(R.drawable.card6));
+		images.add(context.getResources().getDrawable(R.drawable.card7));
+		images.add(context.getResources().getDrawable(R.drawable.card8));
+		images.add(context.getResources().getDrawable(R.drawable.card9));
+		images.add(context.getResources().getDrawable(R.drawable.card10));
+		images.add(context.getResources().getDrawable(R.drawable.card11));
+		images.add(context.getResources().getDrawable(R.drawable.card12));
+		images.add(context.getResources().getDrawable(R.drawable.card13));
+		images.add(context.getResources().getDrawable(R.drawable.card14));
+		images.add(context.getResources().getDrawable(R.drawable.card15));
+		images.add(context.getResources().getDrawable(R.drawable.card16));
+		images.add(context.getResources().getDrawable(R.drawable.card17));
+		images.add(context.getResources().getDrawable(R.drawable.card18));
+		images.add(context.getResources().getDrawable(R.drawable.card19));
+		images.add(context.getResources().getDrawable(R.drawable.card20));
+		images.add(context.getResources().getDrawable(R.drawable.card21));
+		return images;
+	}
+	public Drawable getBackImage(Context context){
+		return context.getResources().getDrawable(R.drawable.back);
+	}
+}
