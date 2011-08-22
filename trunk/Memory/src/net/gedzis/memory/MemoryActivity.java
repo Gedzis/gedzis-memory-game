b395db5e87460f842c4817b20e4d50db9028d975
gedisu@gmail.com <gedisu@gmail.com@60d33090-e776-6669-c13a-5299a5993b29>
2011-08-22 08:49:36 +0000

    Prideta meniu

--- trunk/Memory/src/net/gedzis/memory/MemoryActivity.java
+++ trunk/Memory/src/net/gedzis/memory/MemoryActivity.java
@@ -1,29 +1,16 @@
 package net.gedzis.memory;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import net.gedzis.memory.common.Common;
-import net.gedzis.memory.common.Constants;
-import net.gedzis.memory.model.GameTable;
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 
-public class MemoryActivity extends Activity implements OnClickListener {
-	private Common common = new Common();
-	private List<GameTable> gameTables;
+public class MemoryActivity extends BaseActivity implements OnClickListener {
 
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		gameTables = common.getGameTables();
 		View newButton = findViewById(R.id.new_game_button);
 		newButton.setOnClickListener(this);
 	}
@@ -33,33 +20,7 @@ public class MemoryActivity extends Activity implements OnClickListener {
 		case R.id.new_game_button:
 			openNewGameDialog();
 			break;
-
-		// More buttons go here (if any) ...
 		}
-	}
-
-	private void openNewGameDialog() {
-		ArrayList<String> captions = new ArrayList<String>();
-		for (GameTable gt : gameTables) {
-			captions.add(gt.toString());
-		}
-		new AlertDialog.Builder(this).setTitle(R.string.select_grid_size_title)
-				.setItems(captions.toArray(new CharSequence[captions.size()]),
-						new DialogInterface.OnClickListener() {
-							public void onClick(
-									DialogInterface dialoginterface, int i) {
-								startGame(i);
-							}
-						}).show();
-	}
-
-	public void startGame(int selected) {
-		Intent viewActivityIntent = new Intent(this, CardsGameActivity.class);
-		viewActivityIntent.putExtra(Constants.GAME_TABLE_COL, gameTables.get(
-				selected).getColumns());
-		viewActivityIntent.putExtra(Constants.GAME_TABLE_ROW, gameTables.get(
-				selected).getRows());
-		startActivity(viewActivityIntent);
 	}
 
 }
