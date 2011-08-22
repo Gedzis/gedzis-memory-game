1299fd26a37ea90f4d56a5be3b1ecc1263f81905
gedisu@gmail.com <gedisu@gmail.com>
2011-08-22 08:12:53 +0000

    Pridetas zaidimo lenteles pasirinkimo meniu

---
+++ Memory/src/net/gedzis/memory/common/Constants.java
@@ -0,0 +1,9 @@
+package net.gedzis.memory.common;
+
+public class Constants {
+	public static final int DIFFERENT_IMAGES_AMOUNT = 21;
+	public static final int DEFAULT_GAME_TABLE_SIZE_COL = 3;
+	public static final int DEFAULT_GAME_TABLE_SIZE_ROW = 3;
+	public static final String GAME_TABLE_COL = "game_table_col";
+	public static final String GAME_TABLE_ROW = "game_table_row";
+}
