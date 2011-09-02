3cd60c40617f39c41dd3cdd2e384b907736b741d
gedisu@gmail.com <gedisu@gmail.com>
2011-09-02 08:12:27 +0000

    Fixed #3

--- Memory/src/net/gedzis/memory/activity/LocalHighScoreActivity.java
+++ Memory/src/net/gedzis/memory/activity/LocalHighScoreActivity.java
@@ -11,35 +11,22 @@ import net.gedzis.memory.database.DatabaseCommon;
 import net.gedzis.memory.model.PlayerScore;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 public class LocalHighScoreActivity extends BaseActivity {
-
-	// <TextView android:id="@+id/local_highscore_2x2_caption"
-	// android:layout_width="fill_parent" android:layout_height="wrap_content"
-	// android:text="2x2" android:layout_below="@+id/listTitles" />
-	// <ListView android:id="@+id/high_score_2x2_list"
-	// android:layout_width="fill_parent" android:layout_height="wrap_content"
-	// android:layout_below="@+id/local_highscore_2x2_caption"
-	// android:clickable="false"
-	// android:listSelector="@android:color/transparent" />
-	//
-	// <TextView android:id="@+id/local_highscore_3x3_caption"
-	// android:layout_width="fill_parent" android:layout_height="wrap_content"
-	// android:text="3x3" android:layout_below="@+id/high_score_2x2_list" />
-	// <ListView android:id="@+id/high_score_3x3_list"
-	// android:layout_width="fill_parent" android:layout_height="wrap_content"
-	// android:layout_below="@+id/local_highscore_3x3_caption"
-	// android:clickable="false"
-	// android:listSelector="@android:color/transparent" />
 
 	/** Called when the activity is first created. */
 	private List<PlayerScore> players;
 	private HighScoreArrayAdapter highScoreArrayAdapter;
 	private Database database;
+	private LinearLayout layout;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -54,26 +41,17 @@ public class LocalHighScoreActivity extends BaseActivity {
 		// players = localMemoryXMLParser.parseFile(this);
 		// http://groups.google.com/group/android-beginners/browse_thread/thread/55ee7bee074d3efc/c2407a62aaada5d0?pli=1
 		List<String> tableIds = common.getGameTableIDs();
-		LinearLayout layout = (LinearLayout) findViewById(R.id.high_score_layout);
-		// RelativeLayout header = (RelativeLayout)
-		// findViewById(R.id.list_header2);
+		layout = (LinearLayout) findViewById(R.id.local_high_score_base);
+
+		TableRow tableRow = (TableRow) findViewById(R.id.table_id_list);
+		TableIDClickListener idClickListener = new TableIDClickListener();
 		for (String id : tableIds) {
-			TextView tableCaption = new TextView(layout.getContext());
-			tableCaption.setText(getText(R.string.local_highscore_list_caption)
-					+ id);
-			ListView list = new ListView(layout.getContext());
-			list.setLayoutParams(new LinearLayout.LayoutParams(
-					ViewGroup.LayoutParams.FILL_PARENT,
-					ViewGroup.LayoutParams.WRAP_CONTENT));
-			highScoreArrayAdapter = new HighScoreArrayAdapter(this,
-					R.layout.high_score_element, common
-							.getCurrentTableHighScore(players, id));
-			list.setAdapter(highScoreArrayAdapter);
-			layout.addView(tableCaption);
-
-			layout.addView(list);
-
+			Button tableIdButton = new Button(layout.getContext());
+			tableIdButton.setOnClickListener(idClickListener);
+			tableIdButton.setText(id);
+			tableRow.addView(tableIdButton);
 		}
+		displayTableIdScores(tableIds.get(0));
 		database.close();
 
 	}
@@ -81,6 +59,20 @@ public class LocalHighScoreActivity extends BaseActivity {
 	public List<PlayerScore> getDBRecords() {
 		Cursor c = database.getScores();
 		return DatabaseCommon.getPlayerScoreList(c);
+	}
+
+	public void displayTableIdScores(String tableId) {
+		TextView tableName = (TextView) findViewById(R.id.local_highscore_table_name);
+		tableName.setText(getText(R.string.local_highscore_list_caption) + " "
+				+ tableId);
+		ListView list = (ListView) findViewById(R.id.local_high_score_list);
+		list.setLayoutParams(new LinearLayout.LayoutParams(
+				ViewGroup.LayoutParams.FILL_PARENT,
+				ViewGroup.LayoutParams.WRAP_CONTENT));
+		highScoreArrayAdapter = new HighScoreArrayAdapter(this,
+				R.layout.high_score_element, common.getCurrentTableHighScore(
+						players, tableId));
+		list.setAdapter(highScoreArrayAdapter);
 	}
 
 	public List<PlayerScore> getPlayersDemo() {
@@ -93,23 +85,15 @@ public class LocalHighScoreActivity extends BaseActivity {
 		players.add(new PlayerScore("Tomas1", 5, 34, "5x4"));
 		players.add(new PlayerScore("Tomas2", 5, 34, "1x2"));
 		players.add(new PlayerScore("Marius2", 5, 34, "2x2"));
-		players.add(new PlayerScore("Jonas2", 5, 34, "2x3"));
-		players.add(new PlayerScore("Saulius2", 5, 6666, "3x4"));
-		players.add(new PlayerScore("Mindaugas2", 5, 34, "4x4"));
-		players.add(new PlayerScore("Tomas2", 5, 34, "5x4"));
-		players.add(new PlayerScore("Tomas3", 5, 34, "1x2"));
-		players.add(new PlayerScore("Marius3", 5, 34, "2x2"));
-		players.add(new PlayerScore("Jonas3", 5, 34, "2x3"));
-		players.add(new PlayerScore("Saulius3", 5, 6666, "3x4"));
-		players.add(new PlayerScore("Mindaugas3", 5, 34, "4x4"));
-		players.add(new PlayerScore("Tomas3", 5, 34, "5x4"));
-		players.add(new PlayerScore("Tomas4", 5, 34, "1x2"));
-		players.add(new PlayerScore("Marius4", 5, 34, "2x2"));
-		players.add(new PlayerScore("Jonas4", 5, 34, "2x3"));
-		players.add(new PlayerScore("Saulius4", 5, 6666, "3x4"));
-		players.add(new PlayerScore("Mindaugas4", 5, 34, "4x4"));
-		players.add(new PlayerScore("Tomas4", 5, 34, "5x4"));
-
 		return players;
+	}
+
+	class TableIDClickListener implements OnClickListener {
+
+		public void onClick(View v) {
+			Button tableIdButton = (Button) v;
+			displayTableIdScores(tableIdButton.getText().toString());
+		}
+
 	}
 }
